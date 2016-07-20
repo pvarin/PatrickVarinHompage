412d027432695494fc21ec7fef2573a83589ff7f
Patrick Varin <patrickjvarin@gmail.com>
2016-07-20 19:18:39 +0800

    initial commit: gulp build with browserify, babelify, and react ES6 classes and some small components for the top nav and header

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+
+gulp.task('build', function() {
+	return browserify({
+			entries: ['./src/main.babel'],
+			debug: true
+		}).transform(babelify, {presets:['es2015','react'], extensions:['.babel']})
+		.bundle()
+		.pipe(source('main.js'))
+		.pipe(gulp.dest('./build/'));
+});
